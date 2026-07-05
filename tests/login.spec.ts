78b0cd907ab847df4465a1b6e432f8ea8d3e4079
retadha <retadha.rumi@gmail.com>
2026-07-05 11:18:45 +0700

    add login tc

---
+++ tests/login.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Login Success', async ({ page }) => {
+  await page.goto('https://www.emra.chat/login');
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('testingemrachat@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('tester!3');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await expect(page.getByRole('button', { name: 'Fadhli Maulidri Baru' })).toBeVisible();
+});
