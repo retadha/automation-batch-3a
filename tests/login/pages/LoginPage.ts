6c303469ab57bbeaaa2592ddceeba18d0e2b7dca
retadha <retadha.rumi@gmail.com>
2026-07-18 11:18:52 +0700

    create POM for login

---
+++ tests/login/pages/LoginPage.ts
@@ -0,0 +1,31 @@
+import { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly signInButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.emailInput = page.getByRole('textbox', { name: 'Email' });
+    this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+    this.signInButton = page.getByRole('button', { name: 'Sign In' });
+  }
+
+  async goto() {
+    await this.page.goto('https://www.emra.chat/login');
+  }
+
+  async login(email: string, password: string) {
+    await this.emailInput.click();
+    await this.emailInput.fill(email);
+    await this.passwordInput.click();
+    await this.passwordInput.fill(password);
+    await this.signInButton.click();
+  }
+
+  userMenuButton(name: string): Locator {
+    return this.page.getByRole('button', { name });
+  }
+}
